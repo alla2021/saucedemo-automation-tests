d965e2f62515b1c3341965acfbab4b3a3564a750
Alla Honcharova <alla7gon@gmail.com>
2024-06-13 17:44:06 +0300

    changed .env

--- test/specs/footerLinks_0007.test.js
+++ test/specs/footerLinks_0007.test.js
@@ -1,35 +1,35 @@
-//import { expect } from '@wdio/globals';
-//import LoginPage from '../pageobjects/login.page.js';
-//import InventoryPage from '../pageobjects/inventory.page.js';
+import { expect } from '@wdio/globals';
+import LoginPage from '../pageobjects/login.page.js';
+import InventoryPage from '../pageobjects/inventory.page.js';
 
-//describe('Footer Links', () => {
-//   it('should open Twitter, Facebook, and LinkedIn pages in new tabs', async () => {
-//      await LoginPage.open();
-//      const passwordFieldType = await LoginPage.verifyPasswordFieldType();
-//      expect(passwordFieldType).toEqual('password');      
-//      await LoginPage.login('standard_user', 'secret_sauce');
-//      expect(InventoryPage.footer.toBeDisplayed());
+describe('Footer Links', () => {
+   it('should open Twitter, Facebook, and LinkedIn pages in new tabs', async () => {
+      await LoginPage.open();
+      const passwordFieldType = await LoginPage.verifyPasswordFieldType();
+      expect(passwordFieldType).toEqual('password');      
+      await LoginPage.login(process.env.SAUCEDEMO_USERNAME, process.env.SAUCEDEMO_PASSWORD);
+      expect(InventoryPage.footer.toBeDisplayed());
 
-//      await InventoryPage.twitterLink.click();
-//      const handles = await browser.getWindowHandles();
-//      await browser.switchToWindow(handles[1]);
-//      await expect(browser).toHaveUrl(expect.stringContaining('twitter.com'));
-//      await browser.closeWindow();
-//      await browser.switchToWindow(handles[0]);
+      await InventoryPage.twitterLink.click();
+      const handles = await browser.getWindowHandles();
+      await browser.switchToWindow(handles[1]);
+      await expect(browser).toHaveUrl(expect.stringContaining('twitter.com'));
+      await browser.closeWindow();
+      await browser.switchToWindow(handles[0]);
 
-//      await InventoryPage.facebookLink.click();
-//      const fbHandles = await browser.getWindowHandles();
-//      await browser.switchToWindow(fbHandles[1]);
-//      await expect(browser).toHaveUrl(expect.stringContaining('facebook.com'));
-//      await browser.closeWindow();
-//      await browser.switchToWindow(fbHandles[0]);
+      await InventoryPage.facebookLink.click();
+      const fbHandles = await browser.getWindowHandles();
+      await browser.switchToWindow(fbHandles[1]);
+      await expect(browser).toHaveUrl(expect.stringContaining('facebook.com'));
+      await browser.closeWindow();
+      await browser.switchToWindow(fbHandles[0]);
 
-//      await InventoryPage.linkedinLink.click();
-//      const linkedinHandles = await browser.getWindowHandles();
-//      await browser.switchToWindow(linkedinHandles[1]);
-//      await expect(browser).toHaveUrl(expect.stringContaining('linkedin.com'));
-//      await browser.closeWindow();
-//      await browser.switchToWindow(linkedinHandles[0]);
-//   });
-//});
+      await InventoryPage.linkedinLink.click();
+      const linkedinHandles = await browser.getWindowHandles();
+      await browser.switchToWindow(linkedinHandles[1]);
+      await expect(browser).toHaveUrl(expect.stringContaining('linkedin.com'));
+      await browser.closeWindow();
+      await browser.switchToWindow(linkedinHandles[0]);
+   });
+});
 
--- test/specs/invalidUserName_0003.test.js
+++ test/specs/invalidUserName_0003.test.js
@@ -1,19 +1,19 @@
-//import { expect } from '@wdio/globals'
-//import LoginPage from '../pageobjects/login.page.js'
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page.js'
 
-//describe('Negative Test: Login with Invalid Username', () => {
-//   it('should display error message and highlight fields with invalid Username', async () => {
-//      await LoginPage.open();
-//      const passwordFieldType = await LoginPage.verifyPasswordFieldType();
-//      expect(passwordFieldType).toEqual('password');      
-//      await LoginPage.login('standarD_user', 'secret_sauce');
+describe('Negative Test: Login with Invalid Username', () => {
+   it('should display error message and highlight fields with invalid Username', async () => {
+      await LoginPage.open();
+      const passwordFieldType = await LoginPage.verifyPasswordFieldType();
+      expect(passwordFieldType).toEqual('password');      
+      await LoginPage.login(process.env.NEGATIVE_USERNAME, process.env.SAUCEDEMO_PASSWORD);
 
-//      expect(LoginPage.errorMessage.toBeDisplayed());
+      expect(LoginPage.errorMessage.toBeDisplayed());
 
-//      expect((LoginPage.errorIcons[0]).toBeDisplayed());
-//      expect((LoginPage.errorIcons[1]).toBeDisplayed());
-//      expect(LoginPage.expectErrorMessage('Epic sadface: Username and password do not match any user in this service'));
-//      expect(LoginPage.username).toHaveAttribute('class', expect.stringContaining('error'));
-//      expect(LoginPage.password).toHaveAttribute('class', expect.stringContaining('error'));
-//   });
-//});
+      expect((LoginPage.errorIcons[0]).toBeDisplayed());
+      expect((LoginPage.errorIcons[1]).toBeDisplayed());
+      expect(LoginPage.expectErrorMessage('Epic sadface: Username and password do not match any user in this service'));
+      expect(LoginPage.username).toHaveAttribute('class', expect.stringContaining('error'));
+      expect(LoginPage.password).toHaveAttribute('class', expect.stringContaining('error'));
+   });
+});
